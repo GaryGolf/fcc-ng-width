3db3402eb9258df13df809c7deaa1a9ed355e3fa
GaryGolf <garygolf@mail.ru>
2018-11-12 17:21:59 +0300

    create dimension component

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import * as angular from 'angular';
+import dimension from './dimension.component';
 
 export const AppModule = angular.module('app', [])
+  .component('dimension', dimension)
   .component('appModule', { 
     template: `
       <div data-ng-click="$ctrl.onClick()">
         Hello {{$ctrl.message}}
+        <dimension/>
       </div>
     `,
     controller: function MainModuleController() {
@@ -14,3 +17,5 @@ export const AppModule = angular.module('app', [])
       }
     }
   });
+  
+  
---
+++ src/dimension.component.ts
@@ -0,0 +1,12 @@
+export default {
+  controller: [function () {
+
+  }],
+  template: `
+    <div>
+      <input type="text" data-ng-model="value"/>
+      {{value}}
+    </div>
+  `
+
+}

af692f50f472ef615d3212c0e8db2c17c289fdd7
GaryGolf <garygolf@mail.ru>
2018-12-02 21:58:33 +0300

    update test

--- cypress/integration/dimension_spec.js
+++ cypress/integration/dimension_spec.js
@@ -5,10 +5,18 @@ context('General', () => {
 
   it('input could have auto value', () => {
     // https://on.cypress.io/type
-    cy.get('.input-group .form-control')
+    cy.get('.input-group .form-control', { timeout: 2000 })
       .clear()
+      .focus()
       .type('auto')
       .should('have.value', 'auto')
+  });
+
+  it('input could drop down mwnu', () => {
+    // https://on.cypress.io/type
+    cy.get('.dropdown', { timeout: 2000 })
+      .click()
+      .should('have.class', 'open')
   });
 
 })
